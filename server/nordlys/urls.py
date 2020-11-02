c2d031c4996f80910b7671418325f1243466757a
vinicius Domingos Alves <vinicius.alves@navarratech.com>
2020-11-02 01:39:42 -0300

    Added core urls to main urls

--- server/nordlys/urls.py
+++ server/nordlys/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
+from rest_framework import routers
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('relay/', include('core.urls'))
 ]
